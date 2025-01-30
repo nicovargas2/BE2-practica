bc07d34e4e80fdbda080333dc448cf7f8e53c962
nico vargas <njvnicovargas@gmail.com>
2025-01-29 21:07:33 -0300

    le agrego jwt y GoogleStrategy

---
+++ src/utils/jwt.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+//crear una funcion que reciba un payload y devuelva un token
+export const createToken = (user) => {
+    const { _id, email } = user;
+
+    const payload = { _id, email };
+
+    // el payload es la informacion que quiero guardar en el token
+    // este tiene ya la variable de entorno SECRET
+    // expiresIn es el tiempo de vida del token
+    //return jwt.sign(payload, process.env.SECRET, { expiresIn: '1d' });
+
+    const token = jwt.sign(payload, "codigoSecreto", { expiresIn: '1m' });
+    return token;
+}
+
+//crear una funcion que reciba un token y lo valide
+export const verifyToken = (token) => {
+    try {
+        //return jwt.verify(token, process.env.SECRET);
+        return jwt.verify(token, "codigoSecreto");
+    } catch (error) {
+        return null;
+    }
+}
